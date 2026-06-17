dd655d3599462020cf963b131069b34dd19e53ab
sepfCampus <sebastian.pfeiffer@stud.fh-campuswien.ac.at>
2026-06-17 19:50:03 +0200

    Removed comment

--- backend/src/config/pdfColors.js
+++ backend/src/config/pdfColors.js
@@ -10,13 +10,3 @@ module.exports = {
     MUTED_TEXT: "#44505A",
     BORDER: "#D8E4D8",
 };
-
-
-/*
-
-  static const LIGHT_PRIMARY = Color(0xFF0A736D);
-  static const LIGHT_SECONDARY = Color(0xFFB9ABBD);
-  static const LIGHT_TERTIARY = Color(0xFF6E4E7A);
-  static const LIGHT_QUATERNARY = Color(0xFF049AB0);
-  static const LIGHT_BODY = Color(0xFFF7FBFA);
-*/
